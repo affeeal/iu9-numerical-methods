d16b6e6f28eb8a03a583b99bc4836312f1cd712d
Ilya Afanasyev <affeeal@yandex.ru>
2024-03-25 21:50:29 +0300

    a little refactoring

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,9 +1,8 @@
-#include <algorithm>
 #include <array>
-#include <iostream>
+#include <cmath>
 #include <limits>
 
-namespace {
+namespace details {
 
 constexpr double SqrtNewtonRaphson(const double x, const double curr,
                                    const double prev) {
@@ -11,11 +10,15 @@ constexpr double SqrtNewtonRaphson(const double x, const double curr,
                       : SqrtNewtonRaphson(x, 0.5 * (curr + x / curr), curr);
 }
 
+}  // namespace details
+
+namespace {
+
 // Constexpr version of C++ square root.
 // Source: https://gist.github.com/alexshtf/eb5128b3e3e143187794
 constexpr double Sqrt(const double x) {
   return x >= 0 && x < std::numeric_limits<double>::infinity()
-             ? SqrtNewtonRaphson(x, x, 0)
+             ? details::SqrtNewtonRaphson(x, x, 0)
              : std::numeric_limits<double>::quiet_NaN();
 }
 
@@ -31,37 +34,35 @@ constexpr double HarmonicMean(const double a, const double b) {
   return 2 / (1 / a + 1 / b);
 }
 
+constexpr std::size_t kPoints = 9;
+constexpr std::array<double, kPoints> kXs{
+    1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0,
+};
+constexpr std::array<double, kPoints> kYs{
+    0.16, 0.68, 1.96, 2.79, 3.80, 6.81, 9.50, 15.60, 24.86,
+};
+
+constexpr auto kXA = ArithmeticMean(kXs.front(), kXs.back());  // 3.00
+constexpr auto kXG = GeometricMean(kXs.front(), kXs.back());   // 2.24
+constexpr auto kXH = HarmonicMean(kXs.front(), kXs.back());    // 1.67
+
+constexpr auto kYA = ArithmeticMean(kYs.front(), kYs.back());
+constexpr auto kYG = GeometricMean(kYs.front(), kYs.back());
+constexpr auto kYH = HarmonicMean(kYs.front(), kYs.back());
+
+// Set according to the graph
+constexpr auto kZXA = 4.2;
+constexpr auto kZXG = 1.9;
+constexpr auto kZXH = 0.9;
+
+constexpr std::size_t kDeltasSize = 9;
+constexpr std::array<double, kDeltasSize> kDeltas{
+    std::abs(kZXA - kYA), std::abs(kZXG - kYG), std::abs(kZXA - kYG),
+    std::abs(kZXG - kYA), std::abs(kZXH - kYA), std::abs(kZXA - kYH),
+    std::abs(kZXH - kYH), std::abs(kZXH - kYG), std::abs(kZXG - kYH),
+};
+
 }  // namespace
 
 int main() {
-  constexpr std::size_t kN = 9;
-  constexpr std::array<double, kN> kXs{
-      1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0,
-  };
-
-  constexpr std::array<double, kN> kYs{
-      0.16, 0.68, 1.96, 2.79, 3.80, 6.81, 9.50, 15.60, 24.86,
-  };
-
-  constexpr auto x_a = ArithmeticMean(kXs.front(), kXs.back());  // 3.00
-  constexpr auto x_g = GeometricMean(kXs.front(), kXs.back());   // 2.24
-  constexpr auto x_h = HarmonicMean(kXs.front(), kXs.back());    // 1.67
-
-  constexpr auto y_a = ArithmeticMean(kYs.front(), kYs.back());
-  constexpr auto y_g = GeometricMean(kYs.front(), kYs.back());
-  constexpr auto y_h = HarmonicMean(kYs.front(), kYs.back());
-
-  // Set according to the graph
-  constexpr auto z_x_a = 4.2;
-  constexpr auto z_x_g = 1.9;
-  constexpr auto z_x_h = 0.9;
-
-  constexpr std::size_t kDeltas = 9;
-  constexpr std::array<double, kDeltas> deltas{
-      std::abs(z_x_a - y_a), std::abs(z_x_g - y_g), std::abs(z_x_a - y_g),
-      std::abs(z_x_g - y_a), std::abs(z_x_h - y_a), std::abs(z_x_a - y_h),
-      std::abs(z_x_h - y_h), std::abs(z_x_h - y_g), std::abs(z_x_g - y_h),
-  };
-
-  const auto min_delta = std::min_element(deltas.cbegin(), deltas.cend());
 }
